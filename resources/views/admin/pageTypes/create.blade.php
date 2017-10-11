9ec1948bb85767d2c9c9d60e425596624d166de2
David Crush <dlcrush@mail.roanoke.edu>
2017-10-10 22:38:29 -0400

    making sure everything is committed

---
+++ resources/views/admin/pageTypes/create.blade.php
@@ -0,0 +1,8 @@
+@extends('admin/layouts/app')
+
+@section('content')
+    <h1>{{ $title }}</h1>
+
+    {{! $form }}
+
+@endsection
---
+++ tests/Unit/UrlBuilderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use App\Library\Http\Contracts\UrlBuilder;
+
+class UrlBuilderTest extends TestCase
+{
+    /** @test */
+    public function it_updates_the_path() {
+        //$urlBuilder = create('App\Library\Http\UrlBuilder');
+
+        $this->assertTrue(true);
+    }
+}
