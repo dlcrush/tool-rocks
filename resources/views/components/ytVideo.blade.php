ca67d3b9e244f7251928e32513853d2aea885b52
David L. Crush <david.l.crush@espn.com>
2018-01-19 07:23:32 -0500

    :children_crossing: adding playinsinline=1 to play play videos inline on mobile

--- resources/views/components/ytVideo.blade.php
+++ resources/views/components/ytVideo.blade.php
@@ -1,9 +1,19 @@
+<?php
+
+$queryString = '?enablejsapi=1&origin=http://toolrocks.test&rel=0&showinfo=0&playsinline=1';
+
+if (isset($showControls) && $showControls === false) {
+    $queryString .= '&controls=0';
+}
+
+?>
+
 <iframe
     id="video"
     class="video"
     width="{{ $width }}"
     height="{{ $height }}"
-    src="https://www.youtube.com/embed/{{ $videoId }}?enablejsapi=1&origin=http://toolrocks.dev&rel=0&showinfo=0"
+    src="https://www.youtube.com/embed/{{ $videoId }}{{ $queryString}}"
     frameborder="0"
     allowfullscreen>
 </iframe>
