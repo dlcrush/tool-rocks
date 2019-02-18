65ffacfb542e6bfe75f4c9ee97f8fc0ebcb228d4
David Crush <davidcrushjr@gmail.com>
2019-02-17 21:22:16 -0500

    removing unfinished pages from nav

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -18,14 +18,14 @@
                     <li>
                         <a href="/"><i class="fa fa-home"></i> Home</a>
                     </li>
-                    <li>
+                    {{-- <li>
                         <a href="/tv"><i class="fa fa-tv"></i> TV</a>
-                    </li>
+                    </li> --}}
                     <li class="dropdown">
                         <a href="/videos" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-play"></i> Videos <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="/videos">Browse</a></li>
-                            <li><a href="/videos/search">Search</a></li>
+                            {{-- <li><a href="/videos/search">Search</a></li> --}}
                             <li><a href="/videos?tags=hall-of-fame&page=hall-of-fame">Hall of Fame</a></li>
                             <li><a href="/videos?tags=live-dvd&orderBy=name:desc&page=live-dvd">Live DVDs</a></li>
                         </ul>
